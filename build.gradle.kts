2852839fea68475871fe5223cd6a64bb98767b8f
eymeric <eymericdechelette@gmail.com>
2024-12-16 16:56:08 +0100

    feat: add application config in gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "fr.polytech.suuuuuuuuuuudoku"
@@ -7,6 +8,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass = "fr.polytech.suuuuuuuuuuudoku.Main"
+    applicationDefaultJvmArgs = listOf("-ea")
 }
 
 dependencies {

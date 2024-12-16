b3574bd9315f88d54e35f1e958688f8462200059
eymeric <eymericdechelette@gmail.com>
2024-12-16 17:13:00 +0100

    feat: add solving unit tests

--- src/test/java/fr/polytech/suuuuuuuuuuudoku/ResolveTest.java
+++ src/test/java/fr/polytech/suuuuuuuuuuudoku/ResolveTest.java
@@ -1,34 +1,45 @@
 package fr.polytech.suuuuuuuuuuudoku;
 
+import fr.polytech.suuuuuuuuuuudoku.constraints.AbstractConstraint;
 import fr.polytech.suuuuuuuuuuudoku.constraints.BlockConstraint;
+import fr.polytech.suuuuuuuuuuudoku.constraints.ColumnConstraint;
+import fr.polytech.suuuuuuuuuuudoku.constraints.LineConstraint;
+import fr.polytech.suuuuuuuuuuudoku.solver.SolvingState;
+import fr.polytech.suuuuuuuuuuudoku.solver.SudokuSolver;
+import fr.polytech.suuuuuuuuuuudoku.symbols.SymbolSets;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ResolveTest {
     @Test
-    public void testSolve() {
+    public void testSolveWithoutBackTrack() {
+
+        List<AbstractConstraint> constraintsTests = new ArrayList<>();
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                constraintsTests.add(new BlockConstraint(SymbolSets.DIGITS, i, j, i + 3, j + 3));
+            }
+        }
+        constraintsTests.add(new LineConstraint(SymbolSets.DIGITS));
+        constraintsTests.add(new ColumnConstraint(SymbolSets.DIGITS));
+
+
         var grid = new Grid(new Character[][]{
-                {'5', ' ', ' ', '9', '2', ' ', ' ', '7', ' '},
-                {' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' '},
-                {'1', ' ', ' ', ' ', ' ', ' ', ' ', '5', '6'},
-
-                {'4', '5', '2', ' ', '9', ' ', ' ', ' ', ' '},
-                {'7', ' ', ' ', '3', ' ', '2', ' ', ' ', ' '},
-                {' ', ' ', '9', '5', '6', ' ', ' ', ' ', ' '},
-
-                {' ', ' ', ' ', ' ', '8', ' ', '1', ' ', '2'},
-                {' ', '6', ' ', '7', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '4'}
-        }, List.of(
-                new BlockConstraint(Set.of('1', '2', '3', '4', '5', '6', '7', '8', '9'), 0, 0, 9, 9)
-
-        ));
+                {' ', '9', ' ', ' ', ' ', '2', ' ', '1', ' '},
+                {'2', ' ', '8', ' ', '4', ' ', '9', '3', ' '},
+                {'7', ' ', '3', '1', ' ', '6', '8', ' ', ' '},
+                {' ', ' ', ' ', '3', ' ', ' ', '1', '4', '5'},
+                {'1', '8', '5', ' ', '2', '9', '6', ' ', ' '},
+                {' ', '7', '4', ' ', ' ', '1', '2', ' ', '8'},
+                {' ', ' ', ' ', '2', ' ', ' ', ' ', '8', ' '},
+                {'5', ' ', ' ', '9', ' ', ' ', '7', '6', '2'},
+                {'8', ' ', ' ', '6', ' ', '3', ' ', ' ', ' '}
+        }, constraintsTests);
 
         var res = new Character[][]{
                 {'4', '9', '6', '8', '3', '2', '5', '1', '7'},
@@ -43,11 +54,77 @@ public class ResolveTest {
         };
 
         assertTrue(grid.areConstraintsSatisfied());
+
+        var isSolved = SudokuSolver.solve(grid, SymbolSets.DIGITS, false);
+        assertEquals(SolvingState.SOLVED, isSolved);
+        assertTrue(grid.areConstraintsSatisfied());
+
+        var solvedGrid = grid.getGrid();
+        for (int i = 0; i < solvedGrid.length; i++) {
+            for (int j = 0; j < solvedGrid[i].length; j++) {
+                assertEquals(res[i][j], solvedGrid[i][j]);
+            }
+        }
+    }
+
+    @Test
+    public void testSolveWithBackTrack() {
+
+        List<AbstractConstraint> constraintsTests = new ArrayList<>();
+        for (int i = 0; i < 9; i += 3) {
+            for (int j = 0; j < 9; j += 3) {
+                constraintsTests.add(new BlockConstraint(SymbolSets.DIGITS, i, j, i + 3, j + 3));
+            }
+        }
+        constraintsTests.add(new LineConstraint(SymbolSets.DIGITS));
+        constraintsTests.add(new ColumnConstraint(SymbolSets.DIGITS));
+
+
+        var grid = new Grid(new Character[][]{
+                {'5', ' ', ' ', '9', '2', ' ', ' ', '7', ' '},
+                {' ', ' ', ' ', ' ', ' ', '1', ' ', ' ', ' '},
+                {'1', ' ', ' ', ' ', ' ', ' ', ' ', '5', '6'},
+                {'4', '5', '2', ' ', '9', ' ', ' ', ' ', ' '},
+                {'7', ' ', ' ', '3', ' ', '2', ' ', ' ', ' '},
+                {' ', ' ', '9', '5', '6', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', '8', ' ', '1', ' ', '2'},
+                {' ', '6', ' ', '7', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '4'}
+        }, constraintsTests);
+
+        var res = new Character[][]{
+                {'5', '4', '3', '9', '2', '6', '8', '7', '1',},
+                {'6', '2', '7', '8', '5', '1', '3', '4', '9',},
+                {'1', '9', '8', '4', '7', '3', '2', '5', '6',},
+                {'4', '5', '2', '1', '9', '7', '6', '8', '3',},
+                {'7', '8', '6', '3', '4', '2', '9', '1', '5',},
+                {'3', '1', '9', '5', '6', '8', '4', '2', '7',},
+                {'9', '7', '4', '6', '8', '5', '1', '3', '2',},
+                {'2', '6', '1', '7', '3', '4', '5', '9', '8',},
+                {'8', '3', '5', '2', '1', '9', '7', '6', '4',},
+
+        };
+
+        assertTrue(grid.areConstraintsSatisfied());
+
+        var pastialSolve = SudokuSolver.solve(grid, SymbolSets.DIGITS, false);
+        assertEquals(SolvingState.PARTIALLY_SOLVED, pastialSolve);
+        var isSolved = SudokuSolver.solve(grid, SymbolSets.DIGITS, true);
+        assertEquals(SolvingState.SOLVED, isSolved);
+        assertTrue(grid.areConstraintsSatisfied());
+
+
+        var solvedGrid = grid.getGrid();
+        for (int i = 0; i < solvedGrid.length; i++) {
+            for (int j = 0; j < solvedGrid[i].length; j++) {
+                assertEquals(res[i][j], solvedGrid[i][j]);
+            }
+        }
+
     }
 
     @Test
     public void testSolveFail() {
-        /*
         var grid = new Grid(new Character[][]{
                 {'4', '2', '3'},
                 {'3', '1', '2'},
@@ -56,6 +133,5 @@ public class ResolveTest {
                 new BlockConstraint(Set.of('1', '2', '3'), 0, 0, 2, 2)
         ));
         assertFalse(grid.areConstraintsSatisfied());
-        */
     }
 }

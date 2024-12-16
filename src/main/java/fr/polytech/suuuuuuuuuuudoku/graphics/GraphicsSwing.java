feb8660f9d05b84915c2d6308e52435ab3c8ad20
laracine <thibaut.laracine@etu.univ-lyon1.fr>
2024-12-16 14:46:07 +0100

    add: GraphicsSwing structure for graphics display

---
+++ src/main/java/fr/polytech/suuuuuuuuuuudoku/graphics/GraphicsSwing.java
@@ -0,0 +1,10 @@
+package fr.polytech.suuuuuuuuuuudoku.graphics;
+
+import javax.swing.*;
+
+public class GraphicsSwing extends JFrame {
+
+    private JTable sudoku;
+    private JPanel panel1;
+
+}

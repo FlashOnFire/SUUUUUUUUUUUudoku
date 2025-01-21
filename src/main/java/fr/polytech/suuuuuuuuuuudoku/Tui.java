8cf72591160d55dd0195d0254d07fbc1649d9379
eymeric <eymericdechelette@gmail.com>
2025-01-21 12:22:50 +0100

    refactor: functionalyze the tui

--- src/main/java/fr/polytech/suuuuuuuuuuudoku/Tui.java
+++ src/main/java/fr/polytech/suuuuuuuuuuudoku/Tui.java
@@ -7,29 +7,83 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import fr.polytech.suuuuuuuuuuudoku.algorithm.Generator;
+import fr.polytech.suuuuuuuuuuudoku.grid.Grid;
 
 import java.io.IOException;
 
 public class Tui {
-    static private int line = 0;
+    private final Terminal terminal;
+    private final TextGraphics textGraphics;
+    private int line = 0;
 
-    static void start() throws IOException, InterruptedException {
-        // generer un sudoku, entrer un sudoku
+    public Tui() throws IOException {
         System.setProperty("com.googlecode.lanterna.terminal.UnixTerminal.sttyCommand", "stty");
         DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
-        Terminal terminal = null;
         terminal = defaultTerminalFactory.createTerminal();
-        // Afficher un message de bienvenue
         terminal.setCursorVisible(false);
         terminal.clearScreen();
         terminal.flush();
-        TextGraphics textGraphics = terminal.newTextGraphics();
+        textGraphics = terminal.newTextGraphics();
+    }
+
+    void start() throws IOException, InterruptedException {
+        welcomeMessage();
+        int selectedMode = selectMode();
+        switch (selectedMode) {
+            case 0:
+                // Generer un sudoku
+                //Demander la taille du sudoku
+                int size = selectSize();
+                var grid = Generator.generateClassicNxN(size);
+                displayGrid(grid);
+
+
+                break;
+            case 1:
+                // Entrer un sudoku
+                break;
+        }
+    }
+
+    private void welcomeMessage() throws IOException {
+        // Afficher un message de bienvenue
         textGraphics.setForegroundColor(TextColor.ANSI.GREEN);
         String welcomeMessage = "Bienvenue dans le jeu de Sudoku !";
         textGraphics.putString(0, 0, welcomeMessage);
         line += 2;
         terminal.flush();
+        textGraphics.setForegroundColor(TextColor.ANSI.DEFAULT);
 
+    }
+
+    private void displayGrid(Grid grid) throws IOException {
+        // Afficher la grille
+        grid.display();
+        int gridSize = grid.getGrid().getInner().length;
+        int blockSize = (int) Math.sqrt(gridSize);
+        int spacing = String.valueOf(gridSize).length();
+
+        for (int i = 0; i < gridSize; i++) {
+            if (i % blockSize == 0 && i != 0) {
+                textGraphics.putString(0, line++, "-".repeat(gridSize * (spacing + 1) + blockSize - 1));
+            }
+            for (int j = 0; j < gridSize; j++) {
+                int position = j * (spacing + 1) + j / blockSize;
+                if (j % blockSize == 0 && j != 0) {
+                    textGraphics.putString(position - 1, line, "|");
+                }
+
+                textGraphics.putString(position, line,
+                        grid.getGrid().getInner()[i][j] == null ?
+                                " ".repeat(spacing + 1) :
+                                grid.getGrid().getInner()[i][j] + " ".repeat(spacing));
+            }
+            line++;
+        }
+        terminal.flush();
+    }
+
+    private int selectMode() throws IOException {
         // Afficher un selecteur entre generer un sudoku et entrer un sudoku
         String[] options = {"> Generer un sudoku", "  Entrer un sudoku"};
         for (int i = 0; i < options.length; i++) {
@@ -58,65 +112,57 @@ public class Tui {
             terminal.flush();
         } while (keyStroke.getKeyType() != KeyType.Enter);
         line += options.length + 1;
+        return selectedOption;
+    }
 
-        switch (selectedOption) {
-            case 0:
-                // Generer un sudoku
-                //Demander la taille du sudoku
+    private int selectSize() throws IOException {
+        int selectedSize = 0;
+        int[] possibleSizes = {4, 9, 16, 25, 36, 49, 64, 81, 100};
+        textGraphics.putString(0, line, "Entrez la taille du sudoku");
 
-                textGraphics.putString(0, line, "Entrez la taille du sudoku (4, 9, 16, 25, 36, 49, 64, 81, 100)");
-                line += 1;
-                terminal.flush();
-                int size = 0;
-                do {
-                    keyStroke = terminal.readInput();
-                    if (keyStroke.getKeyType() == KeyType.Character) {
-                        try {
-                            size = Integer.parseInt(keyStroke.getCharacter().toString());
-                            //clean the line
-                            textGraphics.putString(0, line, " ".repeat(terminal.getTerminalSize().getColumns()));
-                            textGraphics.putString(0, line, "Vous avez entré: " + size);
-                        } catch (NumberFormatException e) {
-                            textGraphics.putString(0, line, "Entrée invalide, veuillez entrer un nombre.");
-                        }
-                        terminal.flush();
-                    }
-                } while (size == 0);
-                line += 1;
+        // Afficher un selecteur pour les tailles possibles
+        StringBuilder sizes = new StringBuilder();
+        displaySizes(selectedSize, possibleSizes, sizes);
 
-                var grid = Generator.generateClassicNxN(size);
-
-                //display the grid
-                int gridSize = grid.getGrid().getInner().length;
-                int blockSize = (int) Math.sqrt(gridSize);
-
-                for (int i = 0; i < gridSize; i++) {
-                    if (i % blockSize == 0 && i != 0) {
-                        textGraphics.putString(0, line++, "-".repeat(gridSize * 2 + blockSize - 1));
-                    }
-                    for (int j = 0; j < gridSize; j++) {
-                        if (j % blockSize == 0 && j != 0) {
-                            textGraphics.putString(j * 2 + j / blockSize - 1, line, "|");
-                        }
-                        textGraphics.putString(j * 2 + j / blockSize, line, grid.getGrid().getInner()[i][j] == null ?
-                                "  " :
-                                grid.getGrid().getInner()[i][j] + " ");
-                    }
-                    line++;
+        // Attendre l'input de l'utilisateur
+        KeyStroke keyStroke;
+        do {
+            keyStroke = terminal.readInput();
+            System.out.println(keyStroke.getKeyType());
+            if (keyStroke.getKeyType() == KeyType.ArrowLeft) {
+                if (selectedSize == 0) {
+                    selectedSize = possibleSizes.length - 1;
+                } else {
+                    selectedSize--;
                 }
-                terminal.flush();
-                terminal.flush();
+            } else if (keyStroke.getKeyType() == KeyType.ArrowRight) {
+                if (selectedSize == possibleSizes.length - 1) {
+                    selectedSize = 0;
+                } else {
+                    selectedSize++;
+                }
+            }
 
-                break;
-            case 1:
-                // Entrer un sudoku
-                break;
+            sizes = new StringBuilder();
+            displaySizes(selectedSize, possibleSizes, sizes);
+        } while (keyStroke.getKeyType() != KeyType.Enter);
+        line += 3;
+        return possibleSizes[selectedSize];
+    }
+
+    private void displaySizes(int selectedSize, int[] possibleSizes, StringBuilder sizes) throws IOException {
+        int padding = 0;
+        for (int i = 0; i < possibleSizes.length; i++) {
+            if (i == selectedSize) {
+                textGraphics.setBackgroundColor(TextColor.ANSI.YELLOW);
+                textGraphics.putString(padding, line + 1, String.valueOf(possibleSizes[i]));
+                textGraphics.setBackgroundColor(TextColor.ANSI.DEFAULT);
+            } else {
+                textGraphics.putString(padding, line + 1, String.valueOf(possibleSizes[i]));
+            }
+            padding += String.valueOf(possibleSizes[i]).length() + 1;
         }
-
-
-//        Thread.sleep(2000);
-//        terminal.clearScreen();
-//        terminal.close();
+        terminal.flush();
     }
 }
 

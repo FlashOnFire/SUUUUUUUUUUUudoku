4ae9a43e64fc0ac44728b511e5909d65ba63e745
eymeric <eymericdechelette@gmail.com>
2025-02-01 22:48:41 +0100

    feat: new main for calling others

--- src/main/java/fr/polytech/suuuuuuuuuuudoku/Main.java
+++ src/main/java/fr/polytech/suuuuuuuuuuudoku/Main.java
@@ -4,19 +4,10 @@ import fr.polytech.suuuuuuuuuuudoku.graphics.ImGUIFrame;
 
 import java.io.IOException;
 
-import static imgui.app.Application.launch;
-
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-//        var grid = Generator.generateClassicNxN(16);
-        /*Tui tui = new Tui();
-        tui.start();*/
-//        SwingUtilities.invokeLater(() ->
-//        {
-//            SudokuFrame frame = new SudokuFrame(grid);
-//            frame.setVisible(true);
-//        });
-
-        launch(new ImGUIFrame());
+//        SudokuFrame.main(new String[0]);
+//        Tui.main(new String[0]);
+        ImGUIFrame.main(new String[0]);
     }
 }

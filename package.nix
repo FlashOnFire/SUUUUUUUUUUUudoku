5f7a43e13bc3a0b907beb446e1fc5c3b8cee8465
eymeric <eymericdechelette@gmail.com>
2025-02-06 17:13:36 +0100

    fix: update jar file versions in package.nix to remove SNAPSHOT suffix

--- package.nix
+++ package.nix
@@ -4,7 +4,7 @@
   jdk23,
   gradle,
   makeWrapper,
-  libGL
+  libGL,
 }: let
   jdk = jdk23;
   self = stdenv.mkDerivation (finalAttrs: {
@@ -42,19 +42,19 @@
     installPhase = ''
       export LD_LIBRARY_PATH=${lib.makeLibraryPath [libGL]}
       mkdir -p $out/{bin,share/suuuuuuuuuudoku}
-      cp build/libs/imGUI-1.0-SNAPSHOT.jar $out/share/suuuuuuuuuudoku
-      cp build/libs/swing-1.0-SNAPSHOT.jar $out/share/suuuuuuuuuudoku
-      cp build/libs/tui-1.0-SNAPSHOT.jar $out/share/suuuuuuuuuudoku
+      cp build/libs/imGUI-1.0.jar $out/share/suuuuuuuuuudoku
+      cp build/libs/swing-1.0.jar $out/share/suuuuuuuuuudoku
+      cp build/libs/tui-1.0.jar $out/share/suuuuuuuuuudoku
 
       makeWrapper ${jdk}/bin/java $out/bin/imGUI \
         --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [libGL]} \
-        --add-flags "-jar $out/share/suuuuuuuuuudoku/imGUI-1.0-SNAPSHOT.jar"
+        --add-flags "-jar $out/share/suuuuuuuuuudoku/imGUI-1.0.jar"
 
       makeWrapper ${jdk}/bin/java $out/bin/swing \
-        --add-flags "-jar $out/share/suuuuuuuuuudoku/swing-1.0-SNAPSHOT.jar"
+        --add-flags "-jar $out/share/suuuuuuuuuudoku/swing-1.0.jar"
 
       makeWrapper ${jdk}/bin/java $out/bin/tui \
-        --add-flags "-jar $out/share/suuuuuuuuuudoku/tui-1.0-SNAPSHOT.jar"
+        --add-flags "-jar $out/share/suuuuuuuuuudoku/tui-1.0.jar"
     '';
 
     meta.sourceProvenance = with lib.sourceTypes; [
